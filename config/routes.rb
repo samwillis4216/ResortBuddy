1a384104105b3d167bcdfded7b14e5410e224fc3
Sam Willis <samwilliswork@gmail.com>
2017-11-20 17:25:42 +0000

    added some routes for availibilities, activities and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :employees
   devise_for :guests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :activities do
+    resources :availabilities
+  end
+
+  resources :bookings
+
 end
