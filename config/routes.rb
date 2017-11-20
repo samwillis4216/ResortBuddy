ac3bec1a48b2759cec6358547fb91a39e747536a
Sam Willis <samwilliswork@gmail.com>
2017-11-20 17:33:33 +0000

    created controller for Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'bookings/index'
+
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/edit'
+
+  get 'bookings/update'
+
+  get 'bookings/destroy'
+
   devise_for :employees
   devise_for :guests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
