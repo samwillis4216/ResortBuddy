0f0c8abacae6f6a451979dd7b0b022d3a74428a7
Sam Willis <samwilliswork@gmail.com>
2017-11-21 14:35:02 +0000

    making sign up page responsive with different images

--- app/controllers/guests_controller.rb
+++
@@ -1,9 +0,0 @@
-class GuestsController < ApplicationController
-
-  private
-
-  def guest_params
-    params.require(:guest).permit(:email, :first_name, :last_name, :photo, :photo_cache)
-  end
-
-end
