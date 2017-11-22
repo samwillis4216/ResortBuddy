2bb16fb497d3455e0727d38537a36a43d55956b3
lucas bamber <lucasbamber1@outlook.com>
2017-11-22 10:51:02 +0000

    merge master into login4

--- app/controllers/admin/availabilities_controller.rb
+++ app/controllers/admin/availabilities_controller.rb
@@ -1,3 +1,49 @@
 class Admin::AvailabilitiesController < ApplicationController
 
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.find(params[:activity_id])
+    @availability = Availability.new
+  end
+
+  def create
+    @activity = Activity.find(params[:activity_id])
+    start_time = Time.parse(params[:availability][:start_time])
+    end_time = Time.parse(params[:availability][:end_time])
+    @availability = Availability.new(availability_params)
+    @availability.start_time = start_time
+    @availability.end_time = end_time
+    @availability.activity = @activity
+    @availability.employee = current_employee
+    if @availability.save
+      redirect_to admin_activity_path(@activity)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+    private
+
+  def set_availability
+    @availability = Availability.find(params[:id])
+  end
+
+  def availability_params
+    params.require(:availability).permit(:start_time, :end_time)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,6 @@ Rails.application.routes.draw do
   devise_for :employees
   devise_for :guests
 
-  # Guests
-  resources :activities, only: [:index, :show] do
-    resources :bookings, only: [:index, :create]
-  end
-  resources :bookings, only: [:destroy]
-
   # Employees
   namespace :admin do
     resources :bookings, only: [:index, :show, :edit, :update, :destroy]
@@ -21,4 +15,11 @@ Rails.application.routes.draw do
       resources :availabilities
     end
   end
+
+  # Guests
+  resources :activities, only: [:index, :show] do
+    resources :bookings, only: [:index, :create]
+  end
+  resources :bookings, only: [:destroy]
+
 end
