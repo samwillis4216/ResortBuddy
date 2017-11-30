a6a10a6f11db7a163849c95ed822a47c33053f23
lucas bamber <lucasbamber1@outlook.com>
2017-11-30 14:49:07 +0000

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
 
-config.web_socket_server_url = "wss://resortrunner.herokuapp.com/cable"
-config.action_cable.allowed_request_origins = ['https://resortrunner.herokuapp.com', 'http:/resortrunner.herokuapp.com']
+  config.web_socket_server_url = "wss://resortrunner.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://resortrunner.herokuapp.com', 'http:/resortrunner.herokuapp.com']
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
