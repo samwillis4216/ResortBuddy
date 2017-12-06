050f6ea3499607f697626025aa596a09b7fc33a9
Sam Willis <samwilliswork@gmail.com>
2017-12-06 19:42:16 +0000

    still trying to figure out why my custom fonts aren't being applied

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module ResortBuddy
     # config.action_view.embed_authenticity_token_in_remote_forms = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
