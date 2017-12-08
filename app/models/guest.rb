219adc36acffd703c6f16a6fc97507dd2276f911
Sam Willis <samwilliswork@gmail.com>
2017-12-08 22:02:43 +0000

    bootstrap class for email client-side validation is being applied to the element but bootstraps colors and style is not showing

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,12 @@
 class Guest < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :first_name, :last_name, :email, :password, :message => "All fields must be filled in"
+  validates_length_of :password, :in => 6..20, :message => "A minimum of six characters is required"
+  validates_uniqueness_of :email, :message => "This email has already been taken"
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, :message => "This is not a valid email"
+
+
 
   mount_uploader :photo, PhotoUploader
 
@@ -12,6 +18,5 @@ class Guest < ApplicationRecord
   has_many :messages, as: :messageable
   has_many :received_notifications, as: :notifiable, foreign_key: 'notifiable_id' ,class_name: 'Notification'
   # has_many :sent_notifications, as: :notifierable
-
 
 end
